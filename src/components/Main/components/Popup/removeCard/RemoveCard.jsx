8784b7437f6d49351e0f771d656303b0dd27fe49
Arthur <arthurbarbo177@gmail.com>
2025-08-18 21:25:05 +0100

    hotfix time_2

--- src/components/Main/components/Popup/removeCard/RemoveCard.jsx
+++ src/components/Main/components/Popup/removeCard/RemoveCard.jsx
@@ -1,6 +1,6 @@
 import api from "../../../../../utils/api"
 
-export async function removeCard(cardId, setCards) {
+export async function RemoveCard(cardId, setCards) {
   try {
     await api.deleteCard(cardId);
     setCards((state) => state.filter((c) => c._id !== cardId));
